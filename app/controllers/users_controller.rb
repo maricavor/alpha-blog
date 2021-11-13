05a74e7ea6c22b41032b793e359646344dd2c416
maricavor <sergeit6@gmail.com>
2021-11-13 03:36:25 +0100

    Add user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,11 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
