555c3b2f572a91dab14469bb0bc7e0ffecf45b37
maricavor <sergeit6@gmail.com>
2021-11-13 22:03:19 +0100

    Add color messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
   before_action :require_user, except: [:show, :index]
-  before_action :require_same_user, only: [:edit, :create, :update]
+  before_action :require_same_user, only: [:edit, :update]
   # GET /articles or /articles.json
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
